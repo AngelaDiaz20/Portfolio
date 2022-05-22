8394b8b098aede18c23274d9922e2d06d91e5512
Angela Diaz <angeldi846@gmail.com>
2022-05-22 06:29:02 -0500

    Responsive home settings

--- src/components/presentation/Presentation.js
+++ src/components/presentation/Presentation.js
@@ -19,11 +19,15 @@ function Navbar(){
                         <div className="social-media">
                             Find me on:
                             <div className='social-media__icons'>
-                            <a href="figma.com/@AngelusDiaz" target="_blank" rel="noreferrer" className="link-github"><i class="fa-brands fa-figma"></i></a>
-                            <a href="figma.com/@AngelusDiaz" target="_blank" rel="noreferrer" className="link-github"><i class="fa-brands fa-github-square"></i></a>
-                            <a href="figma.com/@AngelusDiaz" target="_blank" rel="noreferrer" className="link-github"><i class="fa-brands fa-codepen"></i></a>
-                            <a href="figma.com/@AngelusDiaz" target="_blank" rel="noreferrer" className="link-github"><i class="fa-brands fa-behance-square"></i></a>
-                            <a href="figma.com/@AngelusDiaz" target="_blank" rel="noreferrer" className="link-github"><i class="fa-brands fa-linkedin"></i></a></div>
+                                <a href="figma.com/@AngelusDiaz" target="_blank" rel="noreferrer" className="link-figma"><i class="fa-brands fa-figma"></i></a>
+                                
+                                <a href="https://github.com/AngelaDiaz20" target="_blank" rel="noreferrer" className="link-github"><i class="fa-brands fa-github-square"></i></a>
+                                
+                                <a href="https://codepen.io/AngelusDiaz" target="_blank" rel="noreferrer" className="link-codepen"><i class="fa-brands fa-codepen"></i></a>
+                                
+                                <a href="https://www.behance.net/angeladiaz19" target="_blank" rel="noreferrer" className="link-behance"><i class="fa-brands fa-behance-square"></i></a>
+                                
+                                <a href="#" target="_blank" rel="noreferrer" className="link-linkedin"><i class="fa-brands fa-linkedin"></i></a></div>
                         </div>
                     </div>
                 </div>
